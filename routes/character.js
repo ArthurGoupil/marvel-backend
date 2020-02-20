6d1720fab3bbd4d426e403b816bc64098a90d855
ArthurGoupil <arthurgoupil@gmail.com>
2020-02-20 20:45:57 +0100

    added cors

--- routes/character.js
+++ routes/character.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 md5 = require('js-md5');
 const axios = require('axios');
+const cors = require('cors');
+app.use(cors());
 
 const marvelPuk = process.env.MARVEL_PUK;
 const marvelPrk = process.env.MARVEL_PRK;
